8b8fbbde757e4495562ff0d2c54df9de00abe9a4
apratt86 <aaronprattdesign@gmail.com>
2020-06-30 11:13:39 -0400

    Update: corrected css for welcome page and help page, corrected callback menu error for welcome page, Updating readme with appropriate descriptions.

--- admin/partials/admin-functions.php
+++ admin/partials/admin-functions.php
@@ -13,7 +13,7 @@
 require_once plugin_dir_path( __FILE__ ) . 'setup-wizard.php';
 
 function ctf_display_help_page() {
-		echo '<h1 class="ctf-admin-title"><span class="ctf-rainbow">Welcome to ClickUp<img src="'.plugin_dir_url(__DIR__).'css/clickup-symbol_color.png" width="25" height="auto" style="margin: 0px 6px" />Task Forms!</span></h1>';
+		echo '<h1 class="ctf-admin-title"><span class="ctf-rainbow">Setup Guide for ClickUp<img src="'.plugin_dir_url(__DIR__).'css/clickup-symbol_color.png" width="25" height="auto" style="margin: 0px 6px" />Task Forms!</span></h1>';
 		include_once plugin_dir_path( __FILE__ ) . 'help-page.php';
 }
 
@@ -25,4 +25,9 @@ function ctf_display_settings_page() {
 function ctf_sanitize_id( $str = '', $delimiter = '-' ) {
     $slug = strtolower(trim(preg_replace('/[\s-]+/', $delimiter, preg_replace('/[^A-Za-z0-9-]+/', $delimiter, preg_replace('/[&]/', 'and', preg_replace('/[\']/', '', iconv('UTF-8', 'ASCII//TRANSLIT', $str))))), $delimiter));
     return $slug;
+}
+
+function ctf_display_welcome_page() {
+	echo '<h1 class="ctf-admin-title"><span class="ctf-rainbow">Welcome to ClickUp<img src="'.plugin_dir_url(__DIR__).'css/clickup-symbol_color.png" width="25" height="auto" style="margin: 0px 6px" />Task Forms!</span></h1>';
+	echo '<p>For setup instructions please see the <a href="/wp-admin/admin.php?page=ctf_help">Help Page</a>.</p>';
 }
--- wikimotive-clickup-task-forms-free.php
+++ wikimotive-clickup-task-forms-free.php
@@ -29,7 +29,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'CLICKUP_TASK_FORMS_VERSION', '1.0.1' );
+define( 'CLICKUP_TASK_FORMS_VERSION', '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
